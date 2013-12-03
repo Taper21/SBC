c2aeba4ffeac33728631e857dfdceaeae7dcf38f
Sebastian Störchle <sebastian@stoerchle.at>
2013-12-03 21:27:08 +0100

    GUI Interface

---
+++ src/domain/GUIDataManager.java
@@ -0,0 +1,11 @@
+package domain;
+
+import java.util.List;
+
+public interface GUIDataManager {
+	
+	public List<Zutat> getAllMehl();
+	public List<Zutat> getAllEier();
+	public List<Zutat> getAllHonig();
+
+}
