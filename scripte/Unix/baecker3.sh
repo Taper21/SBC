f1bd5dd515d26a08920cee792f9da96b5f703fc1
Sebastian Störchle <sebastian@stoerchle.at>
2014-01-22 00:43:40 +0100

    Skripte

--- scripte/Unix/baecker3.sh
+++ scripte/Unix/baecker3.sh
@@ -1,1 +1,1 @@
-java -cp ./../../target/SBC-0.0.1-SNAPSHOT.jar xvsm.Baecker B3
+java -cp ./../../target/SBC-0.0.1-SNAPSHOT.jar xvsm.Baecker B3 9876
--- scripte/Unix/kontrolleur3.sh
+++ scripte/Unix/kontrolleur3.sh
@@ -1,1 +1,1 @@
-java -cp ./../../target/SBC-0.0.1-SNAPSHOT.jar xvsm.Kontrolleur K3 60
+java -cp ./../../target/SBC-0.0.1-SNAPSHOT.jar xvsm.Kontrolleur K3 60 9876
